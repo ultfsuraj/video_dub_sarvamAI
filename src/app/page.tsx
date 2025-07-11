63bcf3d6fbf07862d3561173880bfb8eb48e763f
R B suraj <rbsuraj432@gmail.com>
2025-07-11 09:29:01 +0530

    delay utility, fix: non-relative @/ import settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,36 @@
 'use client';
 
-import { cn } from '../utils/cn';
+import { cn } from '@/utils/cn';
+import { delay } from '@/utils/delay';
+
+async function handleBoxClick(boxNumber: number) {
+  await delay(1000);
+  alert(`Box ${boxNumber} clicked!`);
+}
 
 function Boxes() {
   return (
     <>
-      <div className="p-2 bg-svm-1 h-20 w-20">-</div>
-      <div className="p-2 bg-svm-1 h-20 w-20">-</div>
-      <div className="p-2 bg-svm-1 h-20 w-20">-</div>
-      <div className="p-2 bg-svm-1 h-20 w-20">-</div>
+      <div onClick={() => handleBoxClick(1)} className="p-2 bg-svm-1 h-20 w-20">
+        1
+      </div>
+      <div onClick={() => handleBoxClick(2)} className="p-2 bg-svm-1 h-20 w-20">
+        2
+      </div>
+      <div onClick={() => handleBoxClick(3)} className="p-2 bg-svm-1 h-20 w-20">
+        3
+      </div>
+      <div onClick={() => handleBoxClick(4)} className="p-2 bg-svm-1 h-20 w-20">
+        4
+      </div>
       <div
+        onClick={() => handleBoxClick(5)}
         className={cn(
           'p-2 bg-svm-1 h-20 w-20',
           `border border-svm-${6} shadow-xl`
         )}
       >
-        -
+        5
       </div>
       <div className="p-2 bg-svm-1 h-20 w-20">-</div>
       <div className="p-2 bg-svm-1 h-20 w-20">-</div>
